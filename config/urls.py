ee30c26e285fb135820799c1dfc07c5f30ac25cf
adubiblessed <adubiblessed@gmail.com>
2025-08-02 01:52:11 +0100

    added work done for taskmanage

--- config/urls.py
+++ config/urls.py
@@ -4,15 +4,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from base.views import home, login, register
-from taskmanage.views import task_display, task_create
+from taskmanage.views import task_display, task_create, toggle_complete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('login/', login, name='login'),
     path('register/', register, name='register'),
-    path('tasks/', task_display, name='tasks'),
+    path('tasks/', task_display, name='tasks_display'),
     path('add_task/', task_create, name='tasks'),
+    path('toggle_complete/<int:task_id>/', toggle_complete, name='toggle_complete'),
 ]
 
 
--- taskmanage/views.py
+++ taskmanage/views.py
@@ -1,24 +1,23 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TaskForm
 from .models import Task
 
-# Create your views here.
 def task_display(request):
     tasks = Task.objects.all()
-     
-    return render(request, 'base/task_display.html', {
-        'tasks': tasks
-    } )
+    return render(request, 'base/task_display.html', {'tasks': tasks})
 
 def task_create(request):
-    form = TaskForm(request.POST)
     if request.method == 'POST':
+        form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('task_display')
+            return redirect('tasks_display')  
     else:
         form = TaskForm()
+    return render(request, 'base/task_create.html', {'form': form})
 
-    context = {'form':form}
-    return render(request, 'base/task_create.html', context)
+def toggle_complete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.completed = not task.completed
+    task.save()
+    return redirect('tasks_display')  
